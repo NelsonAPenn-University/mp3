c0ae13250974fca12d75f3da14837ab881940df5
Nelson Penn <nape226@uky.edu>
2020-10-30 18:49:06 -0400

    q5

--- jenkins/cli/src/main/java/hudson/cli/Connection.java
+++ jenkins/cli/src/main/java/hudson/cli/Connection.java
@@ -135,6 +135,13 @@ public class Connection {
         return buf;
     }
 
+    private KeyPair generateKeyPairWithSpec(DHParameterSpec spec) throws IOException, GeneralSecurityException
+    {
+        KeyPairGenerator dh = KeyPairGenerator.getInstance("DH");
+        dh.initialize(spec);
+        return dh.generateKeyPair();
+    }
+
     /**
      * Performs a Diffie-Hellman key exchange and produce a common secret between two ends of the connection.
      *
@@ -153,9 +160,7 @@ public class Connection {
             AlgorithmParameterGenerator paramGen = AlgorithmParameterGenerator.getInstance("DH");
             paramGen.init(keySize);
 
-            KeyPairGenerator dh = KeyPairGenerator.getInstance("DH");
-            dh.initialize(paramGen.generateParameters().getParameterSpec(DHParameterSpec.class));
-            keyPair = dh.generateKeyPair();
+            keyPair = generateKeyPairWithSpec(paramGen.generateParameters().getParameterSpec(DHParameterSpec.class));
 
             // send a half and get a half
             writeKey(keyPair.getPublic());
@@ -163,9 +168,7 @@ public class Connection {
         } else {
             otherHalf = KeyFactory.getInstance("DH").generatePublic(readKey());
 
-            KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("DH");
-            keyPairGen.initialize(((DHPublicKey) otherHalf).getParams());
-            keyPair = keyPairGen.generateKeyPair();
+            keyPair = generateKeyPairWithSpec( ((DHPublicKey) otherHalf).getParams() );
 
             // send a half and get a half
             writeKey(keyPair.getPublic());
